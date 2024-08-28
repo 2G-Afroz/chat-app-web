6812c04846a703c2bad387dc0d30f99bb0b746f5
2G-Afroz <siddiquiafroz15@gmail.com>
2024-08-29 00:46:13 +0530

    Created route to get all users information.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -80,4 +80,13 @@ const getUserProfile = async (req, res) => {
 	}
 };
 
-export { registerUser, loginUser, getUserProfile };
+const getAllUserProfile = async (req, res) => {
+	try {
+		const users = await User.find().select('-password');
+		res.status(200).json(users);
+	}
+	catch(error) {
+		res.status(500).json({ message: `Server Error: ${error}` });
+	}
+};
+export { registerUser, loginUser, getUserProfile, getAllUserProfile };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { registerUser, loginUser, getUserProfile } from '../controllers/user.controller.js';
+import {
+	registerUser,
+	loginUser,
+	getUserProfile,
+	getAllUserProfile,
+} from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/find/:userId', getUserProfile);
+router.get('/find', getAllUserProfile);
 
 export default router;

f6ee9e766b0d6ce47fd5bfd046d593d23c9b3002
2G-Afroz <siddiquiafroz15@gmail.com>
2024-08-28 19:44:54 +0530

    Created login route.

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -43,8 +43,27 @@ const registerUser = async (req, res) => {
     res.status(201).json({ _id: user._id, name, email, token });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Server Error" });
+    res.status(500).json({ message: `Server Error: ${error}` });
   }
 };
 
-export { registerUser };
+const loginUser = async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		let user = await User.findOne({ email });
+		if (!user) return res.status(400).json({ message: 'Invalid Email or Password.' });
+
+		const isValidPassword = await bcrypt.compare(password, user.password);
+		if(!isValidPassword) return res.status(400).json({ message: 'Invalid Email or Password.' });
+
+		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
+		res.status(200).json({ _id: user._id, name: user.name, email: user.email, token });
+
+	}
+	catch(error) {
+		console.error(error);
+		res.status(500).json({ message: `Server Error: ${error}` });
+	}
+};
+
+export { registerUser, loginUser };

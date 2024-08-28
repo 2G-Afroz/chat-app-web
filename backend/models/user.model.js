c9e57f0ed33bd2623129d5a8d15596299caccd05
2G-Afroz <siddiquiafroz15@gmail.com>
2024-08-28 16:02:44 +0530

    Created user model.

---
+++ backend/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		minlength: 3,
+		maxlength: 30,
+	},
+	email: {
+		type: String,
+		required: true,
+		minlength: 3,
+		maxlength: 30,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 3,
+		maxlength: 30,
+	},
+},
+{
+	timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

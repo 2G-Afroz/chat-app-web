1d8e126bca0959126de54b5b9f3c63d2ce546c1b
2G-Afroz <siddiquiafroz15@gmail.com>
2024-08-28 18:26:55 +0530

    Created basic user route and controller to test.

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+// importing the user route
+import userRoute from './routes/user.route.js';
 
 // Initialize express app
 const app = express();
@@ -20,6 +22,7 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+app.use('/api/user', userRoute);
 app.get('/', (req, res) => {
 	res.send('Welcome to the backend!');
 });

7f1c1f351488929d6ecc2c858ed9a7bfa1e68558
2G-Afroz <siddiquiafroz15@gmail.com>
2024-08-29 10:24:53 +0530

    Created different routes for frontend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Chat from './pages/Chat'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 
 export default function App() {
   return (
-    <div>
-      HELLO
-    </div>
+    <Routes>
+      <Route path="/" element={<Chat />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Navigate to={"/"} />} />
+    </Routes>
   )
 }

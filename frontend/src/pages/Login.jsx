166dc457d3d71c23a10046f172958b2073d60a31
2G-Afroz <siddiquiafroz15@gmail.com>
2024-08-29 21:14:49 +0530

    Completed login functionality.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,9 +2,50 @@ import { Alert, Box, Button, Container, Grid, Grid2, TextField, Typography } fro
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { registerStart, registerSuccess, registerFail } from "../redux/user/userSlice";
 
 export default function Login() {
 	const user = useSelector((state) => state.user.currentUser);
+	const [formData, setFormData] = React.useState({});
+	const [err, setErr] = React.useState();
+	const dispatch = useDispatch();
+
+	const handleChange = (e) => {
+		setFormData({
+			...formData,
+			[e.target.id]: e.target.value,
+		});
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setErr(null);
+		try {
+			dispatch(registerStart());
+			const res = await fetch("/api/user/login", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+
+			if(res.ok) {
+				const resData = await res.json();
+				dispatch(registerSuccess(resData));
+			} else {
+				dispatch(registerFail());
+				const resData = await res.json();
+				setErr(resData.message);
+				return;
+			}
+		} catch (error) {
+			dispatch(registerFail());
+			setErr(error);
+		}
+
+	};
 
 	if(user) {
 		return <Navigate to="/" />;
@@ -23,25 +64,29 @@ export default function Login() {
 				<Typography variant="h5" component="h1">
 					Login
 				</Typography>
-				<Box sx={{ mt: 2 }} component="form">
+				<Box sx={{ mt: 2 }} component="form" onSubmit={handleSubmit}>
 					<Grid2 container spacing={2} direction="column">
 						<Grid2>
 							<TextField
+								id='email'
 								type='email'
 								label="Email"
 								variant="outlined"
 								fullWidth
 								required
 								autoFocus
+								onChange={handleChange}
 							/>
 						</Grid2>
 						<Grid2>
 							<TextField
+								id='password'
 								type='password'
 								label="Password"
 								variant="outlined"
 								fullWidth
 								required
+								onChange={handleChange}
 							/>
 						</Grid2>
 					</Grid2>
@@ -55,9 +100,7 @@ export default function Login() {
 						Login
 					</Button>
 				</Box>
-				<Alert severity="error" sx={{ mt: 2}}>
-					Invalid email or password
-				</Alert>
+				{ err ? <Alert severity="error" sx={{ mt: 2 }}>{err}</Alert> : null }
 			</Box>
 		</Container>
 	)

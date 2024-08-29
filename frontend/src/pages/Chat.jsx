90ce6e498f65c2607585814231cc7e24a0828328
2G-Afroz <siddiquiafroz15@gmail.com>
2024-08-29 11:19:40 +0530

    Added MUI to this app.

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Typography } from '@mui/material'
 
 export default function Chat() {
 	return (
 		<div>
-			Chat
+			<Typography variant="h3">Chat</Typography>
 		</div>
 	)
 }

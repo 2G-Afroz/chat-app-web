92d4cf522a4ab2a707ae2328a534611ae0d8cc85
2G-Afroz <siddiquiafroz15@gmail.com>
2024-08-30 17:11:19 +0530

    Completed Potential Chat Functionality.

---
+++ frontend/src/components/PotentialChat.jsx
@@ -0,0 +1,51 @@
+import { Button } from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import {
+  getChatsStart,
+  getChatsSuccess,
+  getChatsFail,
+} from "../redux/user/chatSlice";
+
+export default function PotentialChat({ user }) {
+  const currentUser = useSelector((state) => state.user.currentUser);
+  const { chats } = useSelector((state) => state.chat);
+  const dispatch = useDispatch();
+
+  const handleCreateChat = async () => {
+    try {
+			//dispatch(getChatsStart());
+      const res = await fetch("/api/chats", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstId: currentUser._id,
+          secondId: user._id,
+        }),
+      });
+
+      if (!res.ok) {
+				//dispatch(getChatsFail());
+        return console.error("Failed to create chat");
+      }
+
+			const data = await res.json();
+			dispatch(getChatsSuccess(chats.concat(data.response)));
+    } catch (err) {
+			//getChatsFail();
+      console.error(err);
+    }
+  };
+
+  return (
+    <Button
+      variant="outlined"
+      style={{ borderRadius: 16 }}
+      onClick={handleCreateChat}>
+      {user.name}
+    </Button>
+  );
+}
--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   getChatsStart,
   getChatsSuccess,
@@ -12,45 +12,86 @@ import {
   Typography,
   Paper,
   List,
-	Button,
+  Button,
+  Badge,
+  Chip,
+  Grid2,
 } from "@mui/material";
+import PotentialChat from "../components/PotentialChat";
 
 export default function Chat() {
   const { chats } = useSelector((state) => state.chat);
   const currentUser = useSelector((state) => state.user.currentUser);
+	const [potentialUsers, setPotentialUsers] = useState([]);
   const dispatch = useDispatch();
 
-	useEffect(() => {
-		getChats();
-	}, []);
+  useEffect(() => {
+    // Function to get All Chats
+    const getChats = async () => {
+      const userId = currentUser._id;
+      try {
+        dispatch(getChatsStart());
+        const res = await fetch(`/api/chats/${userId}`);
 
-  // Function to get All Chats
-  const getChats = async () => {
-    const userId = currentUser._id;
-    try {
-      dispatch(getChatsStart());
-      const res = await fetch(`/api/chats/${userId}`);
-
-      if (res.ok) {
-        const data = await res.json();
-        dispatch(getChatsSuccess(data.chats));
-      } else {
+        if (res.ok) {
+          const data = await res.json();
+          dispatch(getChatsSuccess(data.chats));
+        } else {
+          dispatch(getChatsFail());
+        }
+      } catch (err) {
         dispatch(getChatsFail());
       }
-    } catch (err) {
-      dispatch(getChatsFail());
-    }
-  };
+    };
+    getChats();
+  }, []);
+
+	useEffect(() => {
+		const getPotentialUsers = async () => {
+			try {
+				const res = await fetch('/api/user/find');
+
+				if (!res.ok) {
+					return console.error('Failed to fetch potential users');
+				}
+				
+				const data = await res.json();
+				const pUsers = data.filter((u) => {
+					let isChatCreated = false;
+
+					if(currentUser._id === u._id) {
+						return false;
+					}
+					if(chats) {
+						isChatCreated = chats?.some((chat) => {
+							return chat.members.includes(u._id);
+						});
+					}
+					return !isChatCreated;
+				});
+
+				setPotentialUsers(pUsers);
+			} catch (err) {
+				console.error(err);
+			}
+		}
+		getPotentialUsers();
+	}, [chats]);
 
   return (
     <Box sx={{ display: "flex", height: "89.5vh", mt: 1 }}>
       {/* Left Sidebar - Users List */}
       <Paper sx={{ width: "300px", padding: 2, boxShadow: 3 }}>
-				<List sx={{width: "100%", gap: 1}}>
-					{ chats.map((chat) => (
-						<UserChat key={chat._id} chat={chat} />
+        <Grid2 container gap={"3px"}>
+					{ potentialUsers?.map((user) => (
+						<PotentialChat key={user._id} user={user} />
 					))}
-				</List>
+        </Grid2>
+        <List sx={{ width: "100%", gap: 1 }}>
+          {chats?.map((chat) => (
+            <UserChat key={chat._id} chat={chat} />
+          ))}
+        </List>
       </Paper>
 
       {/* Right Side - Chat Section */}

4920719e9ea956b4c8fb1419d97124c22a9ad83e
2G-Afroz <siddiquiafroz15@gmail.com>
2024-08-31 14:52:31 +0530

    Now, socket returns all the users whenever new user conect or disconnect.

---
+++ socket/index.js
@@ -0,0 +1,20 @@
+import { Server } from "socket.io";
+
+const io = new Server({ cors: "http://localhost:5173" });
+
+let onlineUsers = [];
+
+io.on("connection", (socket) => {
+  socket.on("addNewUser", (userId) => {
+		!onlineUsers.some((user) => user.userId === userId) &&
+    onlineUsers.push({ userId, socketId: socket.id });
+		io.emit("onlineUsers", onlineUsers);
+  });
+
+	socket.on("disconnect", () => {
+		onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+		io.emit("onlineUsers", onlineUsers);
+	});
+});
+
+io.listen(4000);
